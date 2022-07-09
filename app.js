b0958ed2f9b7e3a5a434b289a9ee8244cc9a301d
abhishek singh <abhiknp208016@gmail.com>
2022-07-09 18:31:31 +0530

    f

--- app.js
+++ app.js
@@ -54,6 +54,11 @@ app.use(productRoutes);
 app.use(braintreeRoutes);
 app.use(orderRoutes);
 app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ })
 
 
 const port  = process.env.PORT || 8000 ;
